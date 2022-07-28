c508b89a9ec23b0f7ea7ddcc812775aa869565dd
Moises Amaral <moises.amaral@gmail.com>
2022-07-28 19:23:16 -0300

    Subindo o produto para o Git Hub

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const server = express();
+const port = process.env.PORT || 5050;
+const stock = require('./src/data/stock/stock.json');
+const products = require('./src/data/products/products.json');
+
+server.get('/stock', (req, res) => {
+    return res.json({stock});
+});
+
+server.get('/products', (req, res) => {
+    return res.json({products});
+});
+
+// rodar minha api em produção
+
+
+server.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+}
+);
+
