e801a9d17d150f0c872eae9be9d3092a2e6e3c9d
Moises Amaral <moises.amaral@gmail.com>
2022-07-29 00:44:15 -0300

    Adicionando um Toast no componente login

--- index.js
+++ index.js
@@ -7,11 +7,16 @@ const products = require('./src/data/products/products.json');
 
 server.use(cors());
 
-server.get('/stock', (req, res) => {
+var corsOptions = {
+    origin: 'https://api-rest-full-fruit-fast.vercel.app/',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
+
+server.get('/stock',cors(corsOptions), (req, res, next) => {
     return res.json({stock});
 });
 
-server.get('/products', (req, res) => {
+server.get('/products', cors(corsOptions), (req, res) => {
     return res.json({products});
 });
 
