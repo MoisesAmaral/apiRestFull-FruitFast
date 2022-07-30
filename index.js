fe250eb50ab831266c2cbdc48f3466370e745ae4
Moises Amaral <moises.amaral@gmail.com>
2022-07-30 08:26:12 -0300

    configurando CORS

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
 const express = require('express');
 const cors = require('cors');
-const server = express();
+const app = express();
 const port = process.env.PORT || 5050;
 const stock = require('./src/data/stock/stock.json');
 const products = require('./src/data/products/products.json');
 
-server.use(cors());
+app.use(cors());
 
-var corsOptions = {
-    origin: 'https://fastfruit-ecommerce.vercel.app/',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-  }
 
-server.get('/stock',cors(corsOptions), (req, res, next) => {
+app.get('/stock',(req, res) => {
     return res.json({stock});
 });
 
-server.get('/products', cors(corsOptions), (req, res) => {
+app.get('/products', (req, res) => {
     return res.json({products});
 });
 
 // rodar minha api em produção
 
 
-server.listen(port, () => {
-    console.log(`Server running on port http://localhost:${port}`);
+app.listen(port, () => {
+    console.log(`app running on port http://localhost:${port}`);
 }
 );
 
